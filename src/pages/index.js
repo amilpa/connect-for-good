dd478d492ce2271a04bdc5607db158d374629685
Amil P A <amilpa2020@gmail.com>
2023-10-02 17:54:21 +0530

    remove effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
           {/* End Col */}
           <div className="relative ml-4">
             <img
-              className="rounded-md lg:h-[42rem] lg:w-[55rem]"
+              className="rounded-md lg:h-[45rem] lg:w-[55rem]"
               src="https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&h=800&q=80"
               alt="Image Description"
             />
@@ -63,115 +63,7 @@ export default function Home() {
                 viewBox="0 0 630 451"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x={531}
-                  y={352}
-                  width={99}
-                  height={99}
-                  fill="currentColor"
-                />
-                <rect
-                  x={140}
-                  y={352}
-                  width={106}
-                  height={99}
-                  fill="currentColor"
-                />
-                <rect
-                  x={482}
-                  y={402}
-                  width={64}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect
-                  x={433}
-                  y={402}
-                  width={63}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect
-                  x={384}
-                  y={352}
-                  width={49}
-                  height={50}
-                  fill="currentColor"
-                />
-                <rect
-                  x={531}
-                  y={328}
-                  width={50}
-                  height={50}
-                  fill="currentColor"
-                />
-                <rect
-                  x={99}
-                  y={303}
-                  width={49}
-                  height={58}
-                  fill="currentColor"
-                />
-                <rect
-                  x={99}
-                  y={352}
-                  width={49}
-                  height={50}
-                  fill="currentColor"
-                />
-                <rect
-                  x={99}
-                  y={392}
-                  width={49}
-                  height={59}
-                  fill="currentColor"
-                />
-                <rect
-                  x={44}
-                  y={402}
-                  width={66}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect
-                  x={234}
-                  y={402}
-                  width={62}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect
-                  x={334}
-                  y={303}
-                  width={50}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect x={581} width={49} height={49} fill="currentColor" />
-                <rect x={581} width={49} height={64} fill="currentColor" />
-                <rect
-                  x={482}
-                  y={123}
-                  width={49}
-                  height={49}
-                  fill="currentColor"
-                />
-                <rect
-                  x={507}
-                  y={124}
-                  width={49}
-                  height={24}
-                  fill="currentColor"
-                />
-                <rect
-                  x={531}
-                  y={49}
-                  width={99}
-                  height={99}
-                  fill="currentColor"
-                />
-              </svg>
+              ></svg>
             </div>
             {/* End SVG*/}
           </div>
