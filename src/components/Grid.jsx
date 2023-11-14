5f0fca85fca0db62c159c268724d307f79da3664
amilpa <amilpa2020@gmail.com>
2023-11-14 23:15:57 +0530

    small fix

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,7 +7,6 @@ export default function Grid() {
     async function fetchData() {
       const res = await fetch("/api/event", { method: "GET" });
       const data = await res.json();
-      console.log(data);
       if (!ignore) {
         setEvents(data);
         setLoading(false);
@@ -29,7 +28,7 @@ export default function Grid() {
     );
   }
   return (
-    <div className="mt-12 grid grid-cols-1 place-items-center gap-y-9 md:grid-cols-3">
+    <div className="my-12 grid grid-cols-1 place-items-center gap-y-9 md:grid-cols-3">
       {events.map((event) => {
         return <Card key={event.id} event={event} />;
       })}
