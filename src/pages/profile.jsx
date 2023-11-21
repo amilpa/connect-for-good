8da81b1607f93710497d7930512392bdf8cf72aa
amilpa <amilpa2020@gmail.com>
2023-11-21 21:56:28 +0530

    add update function

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -35,7 +35,6 @@ export default function Profile() {
         }),
       });
       const data = await res.json();
-      console.log(data.data[0]);
       setUserData(data.data[0]);
       if (ignore) setUserData(data.data);
       setLoading(false);
@@ -51,142 +50,200 @@ export default function Profile() {
   if (status === "loading") return null;
   if (status === "unauthenticated") router.push("/");
 
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="mt-8 flex h-[800px] rounded-lg shadow-lg">
-          <div className="relative flex flex-col items-center gap-6 bg-slate-100 px-12 pt-12">
-            {session ? (
-              <img
-                width={40}
-                height={40}
-                className="h-40 w-40 rounded-full"
-                src={session.user.image}
-                alt="profile picture"
-              />
-            ) : null}
-            <Button clickHandle={() => setUpdate(true)} variant={"solid"}>
-              Edit profile
-              <CiEdit />
-            </Button>
-            <div className="mt-4 flex gap-12">
-              <div>
-                <h1 className="text-xl font-semibold">Last event:</h1>
-                <p className="mt-1 text-base">Seven months ago</p>
-              </div>
-              <div>
-                <h1 className="text-xl font-semibold">Location:</h1>
-                <p className="mt-1 text-base">Kochi</p>
-              </div>
-            </div>
-            <div className="mt-4 flex flex-col items-center gap-1">
-              <p className="flex items-center gap-1 text-lg">
-                <SlCalender />
-                Joined 4th September 2023
-              </p>
-              <p className="flex items-center gap-1 text-lg">
-                <FaCheck />
-                35 event completed
-              </p>
-            </div>
-          </div>
-          <div className="h-[800px] flex-grow overflow-y-scroll px-12 py-16">
-            <h1 className="text-2xl font-semibold md:text-4xl">
-              {session.user.role === "organization"
-                ? "Organization details"
-                : "Volunteer details"}
-            </h1>
-            <form className="mt-4 flex flex-col gap-6">
-              {session.user.role === "organization" ? (
-                <>
-                  <Input
-                    name={"Name"}
-                    value={userData.name}
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Email"}
-                    value={userData.email}
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Address"}
-                    value={
-                      userData.experience ? userData.experience : "(Not given)"
-                    }
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Goals"}
-                    value={
-                      userData.education ? userData.education : "(Not given)"
-                    }
-                    readonly={!update}
-                  />
-                  <TextArea
-                    name={"Description"}
-                    value={userData.about ? userData.about : "(Not given)"}
-                    readonly={!update}
-                  />
-                </>
-              ) : (
-                <>
-                  <Input
-                    name={"Name"}
-                    value={userData.name}
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Email"}
-                    value={userData.email}
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Experience"}
-                    value={
-                      userData.experience ? userData.experience : "(Not given)"
-                    }
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Education"}
-                    value={
-                      userData.education ? userData.education : "(Not given)"
-                    }
-                    readonly={!update}
-                  />
-                  <Input
-                    name={"Occupation"}
-                    value={
-                      userData.occupation ? userData.occupation : "(Not given)"
-                    }
-                    readonly={!update}
-                  />
-                  <TextArea
-                    name={"About"}
-                    value={userData.about ? userData.about : "(Not given)"}
-                    readonly={!update}
-                  />
-                </>
-              )}
+  function handleEdit() {
+    setUpdate(true);
+  }
 
-              {update ? (
-                <Button
-                  variant={"solid"}
-                  classNames={"mt-4"}
-                  // clickHandle={clickHandle}
-                >
-                  Submit
-                </Button>
+  async function handleUpdate() {
+    setLoading(true);
+    const res = await fetch("/api/user", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        role: session.user.role,
+        email: session.user.email,
+        data: userData,
+      }),
+    });
+    setLoading(false);
+    setUpdate(false);
+  }
+
+  if (status === "authenticated" && userData) {
+    return (
+      <>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="mt-8 flex h-[800px] rounded-lg shadow-lg">
+            <div className="relative flex flex-col items-center gap-6 bg-slate-100 px-12 pt-12">
+              {session ? (
+                <img
+                  width={40}
+                  height={40}
+                  className="h-40 w-40 rounded-full"
+                  src={session.user.image}
+                  alt="profile picture"
+                />
               ) : null}
-            </form>
+              <Button clickHandle={handleEdit} variant={"solid"}>
+                Edit profile
+                <CiEdit />
+              </Button>
+              <div className="mt-4 flex gap-12">
+                <div>
+                  <h1 className="text-xl font-semibold">Last event:</h1>
+                  <p className="mt-1 text-base">Seven months ago</p>
+                </div>
+                <div>
+                  <h1 className="text-xl font-semibold">Location:</h1>
+                  <p className="mt-1 text-base">Kochi</p>
+                </div>
+              </div>
+              <div className="mt-4 flex flex-col items-center gap-1">
+                <p className="flex items-center gap-1 text-lg">
+                  <SlCalender />
+                  Joined 4th September 2023
+                </p>
+                <p className="flex items-center gap-1 text-lg">
+                  <FaCheck />
+                  35 event completed
+                </p>
+              </div>
+            </div>
+            <div className="h-[800px] flex-grow overflow-y-scroll px-12 py-16">
+              <h1 className="text-2xl font-semibold md:text-4xl">
+                {session.user.role === "organization"
+                  ? "Organization details"
+                  : "Volunteer details"}
+              </h1>
+              <form className="mt-4 flex flex-col gap-6">
+                {session.user.role === "organization" ? (
+                  <>
+                    <Input
+                      name={"Name"}
+                      value={userData.name}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, name: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Email"}
+                      value={userData.email}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, email: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Address"}
+                      value={
+                        userData.address ? userData.address : "(Not given)"
+                      }
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, address: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Goals"}
+                      value={userData.goals ? userData.goals : "(Not given)"}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, goals: e.target.value })
+                      }
+                    />
+                    <TextArea
+                      name={"Description"}
+                      value={userData.about ? userData.about : "(Not given)"}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, about: e.target.value })
+                      }
+                    />
+                  </>
+                ) : (
+                  <>
+                    <Input
+                      name={"Name"}
+                      value={userData.name}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, name: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Email"}
+                      value={userData.email}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, email: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Experience"}
+                      value={
+                        userData.experience
+                          ? userData.experience
+                          : "(Not given)"
+                      }
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, experience: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Education"}
+                      value={
+                        userData.education ? userData.education : "(Not given)"
+                      }
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, education: e.target.value })
+                      }
+                    />
+                    <Input
+                      name={"Occupation"}
+                      value={
+                        userData.occupation
+                          ? userData.occupation
+                          : "(Not given)"
+                      }
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, occupation: e.target.value })
+                      }
+                    />
+                    <TextArea
+                      name={"About"}
+                      value={userData.about ? userData.about : "(Not given)"}
+                      readonly={!update}
+                      onchange={(e) =>
+                        setUserData({ ...userData, about: e.target.value })
+                      }
+                    />
+                  </>
+                )}
+
+                {update ? (
+                  <Button
+                    variant={"solid"}
+                    classNames={"mt-4"}
+                    clickHandle={handleUpdate}
+                  >
+                    Submit
+                  </Button>
+                ) : null}
+              </form>
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
+        )}
+      </>
+    );
+  }
 }
 
 Profile.getLayout = function getLayout(page) {
